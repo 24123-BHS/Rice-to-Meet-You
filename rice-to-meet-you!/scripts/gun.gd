92c65988224dfdcc05b3b4fef0741cdfa747d3f2
24123-BHS <24123@burnside.school.nz>
2026-06-02 09:22:59 +1200

    working on the gun

--- rice-to-meet-you!/scripts/gun.gd
+++ rice-to-meet-you!/scripts/gun.gd
@@ -1,26 +1,26 @@
 extends Node2D
 
-
-const BULLET = preload("res://prefabs/bullet.tscn")
-
-@onready var muzzle: Marker2D = $Marker2D
+@onready var rotation_offset: Node2D = $RotationOffset
+@onready var shadow: Sprite2D = $RotationOffset/Sprite2D/shadow
+@onready var shoot_pos: Marker2D = $RotationOffset/Sprite2D/shoot_pos
 
 var time_between_shot: float = 0.25
 var can_shoot: bool = true
 
+func _ready() -> void:
+	$ShootTimer.wait_time = time_between_shot
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	look_at(get_global_mouse_position())
-
-	rotation_degrees = wrap(rotation_degrees, 0, 360)
-	if rotation_degrees > 90 and rotation_degrees < 270:
-		scale.y = -1
-	else:
-		scale.y = 1
+	rotation_offset.rotation = lerp_angle(rotation_offset.rotation, ( get_global_mouse_position() - global_position ).angle(), 6.5*delta)
+	shadow.position = Vector2(-2, 2).rotated(-rotation_offset.rotation)
+	
+	if Input.is_action_just_pressed("shoot") and can_shoot:
+		can_shoot = false
+		$ShootTimer.start()
 		
-	if Input.is_action_just_pressed("shoot"):
-		var bullet_instance = BULLET.instantiate()
-		get_tree().root.add_child(bullet_instance)
-		bullet_instance.global_position = muzzle.global_position
-		bullet_instance.rotation = rotation
+func _shoot():
+	pass
+
+func _on_shoot_timer_timeout() -> void:
+	can_shoot = true
